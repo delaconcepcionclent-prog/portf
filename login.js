d0af146b95bbd8c3a449a8cfd03ade6bca20aa0b
delaconcepcionclent-prog <delaconcepcionclent@gmail.com>
2026-07-20 16:15:37 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,31 @@
+const USERNAME = "clent";
+const PASSWORD = "portfolio123";
+
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    if(
+        username === USERNAME &&
+        password === PASSWORD
+    ){
+
+        localStorage.setItem("loggedIn","true");
+
+        window.location.href = "index.html";
+
+    }else{
+
+        document.getElementById("error").textContent =
+        "Incorrect username or password.";
+    }
+
+});
